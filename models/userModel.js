8dbb7b15375243a3ff2117706bc9056ce65f2024
chaudharyvicky564@gmail.com <chaudharyvicky564@gmail.com>
2024-09-15 23:16:33 +0530

    stage Base

---
+++ models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://127.0.0.1:27017/Final-nodejs-project").then((res)=> console.log("DB connected"))
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String
+    },
+    password:{
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model("user", userSchema)
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+app.get("/", (req,res)=>{})
+
+app.use("/", require("./routes/userRoutes"))
+
+app.listen(3000, ()=> console.log("server running on port 3000"))
