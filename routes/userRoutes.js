8b66c016753e28a84fdac1a66a937d1b63d28a7b
chaudharyvicky564@gmail.com <chaudharyvicky564@gmail.com>
2024-09-16 23:47:46 +0530

    ejs partials done, conditional rendering of sidebar for settings and dashboard done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,7 +14,18 @@ router.get("/login", (req,res)=>{
 router.get("/dashboard",async (req,res)=>{
     let user = await userModel.findById(req.session.userId)
     let posts = await postModel.find()
-    res.render("dashboard")
+    res.render("dashboard", {currentRoute: 'dashboard'})
+})
+
+router.get("/profile",async (req,res)=>{
+    let user = await userModel.findById(req.session.userId)
+    let posts = await postModel.find()
+    res.render("profile", {currentRoute : 'profile'})
+})
+router.get("/settings",async (req,res)=>{
+    let user = await userModel.findById(req.session.userId)
+    let posts = await postModel.find()
+    res.render("settings", {currentRoute: 'settings'})
 })
 
 module.exports = router
